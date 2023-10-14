c192a0a3311e3157064f0737270553f04900554c
ElKyMo <ilyamalyhin@gmail.com>
2023-10-14 10:43:13 +0200

    Class added, prot. function added

--- Lesson141023/Source.cpp
+++ Lesson141023/Source.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+
+class Products
+{
+public:
+
+
+private:
+	string name;
+	string description;
+	int price;
+	int quantity_in_warehouse;
+
+};
+
+class Bascket : public Products
+{
+public:
+
+
+private:
+	vector<Products> productList;
+};
+
+void Order();
+
+class Users
+{
+public:
+
+
+private:
+	string login;
+	string password;
+};
+
+void Payment();
+
 
 int main()
 {
